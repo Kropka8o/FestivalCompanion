cfa840fa8cf0fa68493ee57b9c52f719aaa607bb
Kropka8o <kropka.koniec.switata@gmail.com>
2025-11-24 09:58:36 +0100

    Pogin gedaan tot login systeem opzetten.

---
+++ FestivalCompanion/Data/AppDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+namespace FestivalCompanion.Data
+{
+    public class AppDBContext: IdentityDbContext
+    {
+        public DbSet<Models.AccountViewModel> Account { get; set; }
+
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseMySql(
+        //        "server=localhost;port=3306;user=root;database=bloodhound;",
+        //        ServerVersion.Parse("8.0.44")
+        //        );
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+    }
+}
--- FestivalCompanion/Program.cs
+++ FestivalCompanion/Program.cs
@@ -1,8 +1,23 @@
+using FestivalCompanion.Customs;
+using FestivalCompanion.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Bloodhound") ?? throw new InvalidOperationException("Connection string 'Bloodhound' not found.");
+
+builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(connectionString));
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDBContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+   .AddEntityFrameworkStores<AppDBContext>() ;
+//.addDefaultTokenProviders();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,10 +33,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapRazorPages();
 app.Run();
