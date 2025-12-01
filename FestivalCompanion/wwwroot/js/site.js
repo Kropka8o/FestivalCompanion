84c471941c2ce3b4f2ebf98390a0fa690e74fe0a
bwalting <babita.alting@hotmail.com>
2025-12-01 13:15:26 +0100

    Layout aangepast en basis styling toegevoegd

--- FestivalCompanion/wwwroot/js/site.js
+++ FestivalCompanion/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const introscherm = document.getElementById("introscherm");
+const keuzescherm = document.getElementById("keuzescherm");
+
+setTimeout(() => {
+    introscherm.style.opacity = 0;
+
+    introscherm.addEventListener("transitionend", () => {
+        introscherm.style.display = "none";
+        keuzescherm.style.display = "flex";
+    }, { once: true });
+}, 2500);
